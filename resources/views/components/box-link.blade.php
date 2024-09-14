44719f0bccd131561fe33a2b9ec1c37d43b09254
Iago Melo <iagooliveiradev@outlook.com>
2024-09-14 01:12:48 -0300

    style: removendo classe não usada

--- resources/views/components/box-link.blade.php
+++ resources/views/components/box-link.blade.php
@@ -28,7 +28,7 @@
                 <div class="ms-3 text-sm font-medium" id="box-alert-message-{{$key}}">
                   
                 </div>
-                <button type="button" onclick="closeAlert({{$key}})" class="ms-auto -mx-1.5 -my-1.5 bg-yellow-50 text-yellow-500 rounded-lg focus:ring-2 focus:ring-yellow-400 p-1.5 hover:bg-yellow-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-yellow-300 dark:hover:bg-gray-700" data-dismiss-target="#alert-4" aria-label="Close">
+                <button type="button" onclick="closeAlert({{$key}})" class="ms-auto -mx-1.5 -my-1.5 bg-yellow-50 text-yellow-500 rounded-lg focus:ring-2 focus:ring-yellow-400 p-1.5 hover:bg-yellow-200 inline-flex items-center justify-center h-8 w-8" aria-label="Close">
                   <span class="sr-only">Close</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
