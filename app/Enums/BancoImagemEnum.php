a5c4d28c3df389e49859d47953d1c7cc8cd383e7
Iago Melo <iagooliveiradev@outlook.com>
2024-10-03 22:15:50 -0300

    feat: criando enum para validar os bancos

---
+++ app/Enums/BancoImagemEnum.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace App\Enums;
+
+enum BancoImagemEnum: int
+{
+    case istock = 0;
+    case shutterstock = 1;
+    case freepik = 2;
+
+
+     // Método para retornar informações extras
+ public function getDescription(): string
+ {
+     return match($this) {
+         self::istock => 'istock',
+         self::shutterstock => 'shutterstock',
+         self::freepik => 'freepik',
+     };
+ }
+ public function getVideoCondition(): string
+ {
+     return match($this) {
+         self::istock => true,
+         self::shutterstock => true,
+         self::freepik => true,
+     };
+ }
+ public function getVideoDescription(): string
+ {
+     return match($this) {
+         self::istock => '/video/',
+         self::shutterstock => '/video/',
+         self::freepik => '/video/',
+     };
+ }
+
+ public function getUrl(): string
+ {
+     return match($this) {
+         self::istock => 'https://www.istockphoto.com',
+         self::shutterstock => 'https://www.shutterstock.com',
+         self::freepik => 'https://www.freepik.com',
+     };
+ }
+
+} 
