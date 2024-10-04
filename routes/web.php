202c5d2869a8f2ed9367bf259530a43e16fa23fd
Iago Melo <iagooliveiradev@outlook.com>
2024-10-03 22:16:06 -0300

    redirecionando para login

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/teste', function () {
     return view('welcome');
