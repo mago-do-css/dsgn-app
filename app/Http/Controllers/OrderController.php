ab531bf0d819dcf3e6d7aa0883c60a7736889993
Iago Melo <iagooliveiradev@outlook.com>
2024-10-27 21:15:44 -0300

    wipo: criando histórico de downloads

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,8 +7,11 @@ use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Http; 
 use Illuminate\Validation\Rule;
 use App\Enums\BancoImagemEnum;
+use App\Models\DownloadHistorical;
 
 class OrderController extends Controller
 {
@@ -23,7 +26,7 @@ class OrderController extends Controller
         try{ 
             $request->validate([
                 'stock_url'=> 'required', 
-                'isPreview'=> 'required',
+                'isPreview'=> 'required|in:true,false',
             ]);
             
             $this->orderService->requestValidator($request); 
@@ -62,4 +65,48 @@ class OrderController extends Controller
         // Processar a resposta conforme necessário
         return $responseBody;
     } 
+
+    public function getImagesByFilter(Request $request){
+
+        // $request->validate([
+        //     'filter'=> 'required|in:free,stock',
+        // ]);
+
+        $getHistorical = DownloadHistorical::query();
+
+        if(!empty($request->imageOrigin)){ 
+            foreach ($request->image_origin as $image_bank) { 
+                if (!is_null($image_bank)) {
+                    $getHistorical->where('image_bank', $image_bank);
+                }
+            } 
+        }
+
+        if(!empty($request->date)){ 
+            $getHistorical->where('date', '>=',$request->min_date)
+            ->and('date', '<=',$request->max_date);
+        }
+
+        $getHistorical = $getHistorical->get();
+
+        return $getHistorical;
+    }
+
+
+    public function teste(){
+        // adicionar o parametro $bancoImagem
+            //TODO: para testar
+            $url = "https://www.shutterstock.com/pt/image-photo/smiling-30s-latin-hispanic-middleaged-business-2491646071";
+
+            $encodedUrl = urlencode($url); 
+        
+            $endpoint = "https://vip.neh.tw/api/stockinfo/shutterstock/null?url={$encodedUrl}"; 
+
+            $response = Http::withHeaders([
+                'X-Api-Key' => 'sV6mS2Q3NArE2s351IXovmEOcXaSwk',
+            ])->get($endpoint); 
+ 
+           
+            dd($response->json());
+    }
 }
---
+++ app/Models/DownloadHistorical.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DownloadHistorical extends Model
+{
+    use HasFactory;
+    
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'image_path',
+        'image_name',
+        'image_bank',
+        'date', 
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
