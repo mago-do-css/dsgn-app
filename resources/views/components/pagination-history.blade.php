13d9596e0eb9f5d74b2e347066efc6f8e858e7f5
Iago Melo <iagooliveiradev@outlook.com>
2024-10-30 21:44:48 -0300

    feat: mudar de página levando parametros

--- resources/views/components/pagination-history.blade.php
+++ resources/views/components/pagination-history.blade.php
@@ -9,19 +9,40 @@
             @for ($i = $paginationData['startNumber']; $i <= $paginationData['endNumber']; $i++)
                 <li>
                     <a @if ($i == $page) aria-current="page" @endif
-                        href="{{ route('history', ['page' => $i]) }}"
-                        class=" flex items-center justify-center px-3 h-8 leading-tight 
+                        {{-- href="{{ route('history', ['page' => $i]) }}" --}}
+                        onclick="updatePagination({{ $i }})"
+                        class="cursor-pointer flex items-center justify-center px-3 h-8 leading-tight 
                           @if ($i == $page) text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 
                           @else text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 @endif 
                         ">
                         {{ $i }}
                     </a>
                 </li>
-            @endfor
-
+            @endfor 
             <li>
                 <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
             </li>
         </ul>
     </nav>
 </div>
+<script>
+    function updatePagination(newPage) {
+         
+    // Obtém a URL atual do navegador
+    const currentUrl = window.location.href;
+
+    // Cria um objeto URL a partir da URL atual
+    const urlObj = new URL(currentUrl);
+
+    // Obtém os parâmetros existentes
+    const params = new URLSearchParams(urlObj.search);
+
+    // Atualiza ou adiciona o parâmetro de página
+    params.set('page', newPage);
+
+    // Constrói a nova URL
+    urlObj.search = params.toString();
+    
+    window.location.href = urlObj.toString();
+}
+</script>
