bef3553bc5ec95f06ac3be584033ebbfe51e03af
Iago Melo <iagooliveiradev@outlook.com>
2024-11-03 17:17:29 -0300

    feat: parametro de oder code

--- resources/views/components/box-link.blade.php
+++ resources/views/components/box-link.blade.php
@@ -100,6 +100,8 @@
             <form id="form-shutter-{{ $key }}" class="flex items-center">
                 @csrf
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <span id="order-code-render-{{ $key }}">
+              </span> 
                 <span
                     class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg ">URL</span>
                 <div class="relative w-full"> <input autocomplete="off" id="stock-url-{{ $key }}" name="stock_url" type="text"
