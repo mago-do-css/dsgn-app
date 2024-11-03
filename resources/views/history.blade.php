4a2f500ca42f50ef0ae484d52a6f1c75251df42a
Iago Melo <iagooliveiradev@outlook.com>
2024-11-03 00:42:12 -0300

    wip: filtrando por nome com filtros avançados junto

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -158,13 +158,46 @@
                     </div>
                 </form>
             </div>
-            <div class="w-[1200px] grid grid-rows-3 grid-flow-col gap-4">
+            <div>
+                <form class="max-w-2xl mx-auto mb-4" action="{{ route('history', ['page' => 1]) }}"  method="GET">   
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Pesquisar</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        @if ($selectedOptionsImageBank != null)
+                            @foreach ($selectedOptionsImageBank as $option)
+                                <input type="hidden" name="stocks_origin[]" value="{{ $option }}">
+                            @endforeach
+                        @endif
+                        @if ($selectedOptionsStockType != null)
+                            @foreach ($selectedOptionsStockType as $type)
+                                <input type="hidden" name="stocks_type[]" value="{{ $type }}">
+                            @endforeach
+                        @endif
+                       
+                        <input type="search" 
+                        id="default-search" 
+                        class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                        placeholder="Procure por Mockups, Logos..." 
+                        required 
+                        name="search"
+                        />
+                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesquisar</button>
+                    </div>
+                </form>
+
+                 <div class="w-[1200px] grid grid-rows-3 grid-flow-col gap-4"> 
                 @foreach ($historyData as $key => $content)
                     <x-box-image-history :image="$content['stock_url']" :imageOrigin="$content['stock_origin']" :key="$key" />
                 @endforeach
             </div>
+            </div>
+           
         </div>
-        <div class="max-w-7xl mx-auto flex justify-center mt-8">
+        <div class="max-w-7xl mx-auto flex justify-center mt-8"> 
             <x-pagination-history :page="$page" :paginationData="$paginationData" />
         </div>
     </div> 
