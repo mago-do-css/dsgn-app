e8d4b48e25aed3d310d6b222f97cae24263ebc40
Elber Afonso <elber.afonso@outlook.com>
2024-11-06 15:20:52 -0300

    feat: loader para o contador de downloads

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,8 +39,18 @@
                             <path
                                 d="M12.707 9.293a1 1 0 0 0-1.414 0l-2 2a1 1 0 1 0 1.414 1.414l.293-.293V19a1 1 0 1 0 2 0v-6.586l.293.293a1 1 0 0 0 1.414-1.414l-2-2Z" />
                         </svg>
-                        Limite <div class="flex ml-1"><span id="actualLimit"></span>/<span id="totalLimit"></span></div>
-                    </a></form>
+                        Limite
+                        <div class="flex ml-1">
+                            <svg id="loader" class="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="white"/>
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                            </svg>
+                            <span id="limitDisplay">
+                                <span id="actualLimit"></span>/<span id="totalLimit"></span>
+                            </span>
+                        </div>
+                    </a>
+                </form>
                 <div data-popover id="popover-bottom" role="tooltip"
                     class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
                     <div class="px-3 py-2 cursor-default">
@@ -158,14 +168,12 @@
     document.addEventListener('DOMContentLoaded', limitCounter());
 
     function limitCounter() {
-        //Inicia a requisição
         const xhttp = new XMLHttpRequest();
-
-        //Define a rota 
         const actionUrl = @json(route('userLimit'));
-
         let actualLimit = document.getElementById('actualLimit');
         let totalLimit = document.getElementById('totalLimit');
+        let loader = document.getElementById('loader');
+        let limitDisplay = document.getElementById('limitDisplay');
         var form = document.getElementById('limitCounter');
         var token = form.querySelector('input[name="_token"]').value;
 
@@ -173,37 +181,36 @@
         xhttp.setRequestHeader("Content-Type", "application/json");
         xhttp.setRequestHeader("X-CSRF-TOKEN", token);
 
-        xhttp.onload = function() {
-            console.log(this);
+        // Exibe o loader e oculta o display do limite
+        loader.style.display = 'inline-block';
+        limitDisplay.style.display = 'none';
 
+        xhttp.onload = function() {
             let response;
 
             try {
                 response = JSON.parse(this.responseText);
             } catch (error) {
-                // Trata o erro de JSON inválido 
                 response = {
                     status: false,
                     message: "Resposta do servidor está inválida. Contacte o suporte!"
                 };
             }
 
-            //console.log(response);
+            // Atualiza os valores de limite
             actualLimit.innerHTML = response.actualLimit;
             totalLimit.innerHTML = response.totalLimit;
 
-        }
+            // Oculta o loader e exibe o display do limite após carregar os dados
+            loader.style.display = 'none';
+            limitDisplay.style.display = 'inline';
+        };
 
         xhttp.onerror = function() {
             console.error('Erro:' + this.message);
+            loader.style.display = 'none';
+            limitDisplay.style.display = 'inline';
         };
-
-
-        // xhttp.onreadystatechange = function() {
-        //     if (this.readyState === XMLHttpRequest.DONE) {
-        //         console.log(this);
-        //     }
-        // };
 
         xhttp.send();
     }
