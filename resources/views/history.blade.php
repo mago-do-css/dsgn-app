ab259944dd62d9bad50008dc57c2ea07bd0a69e3
Iago Melo <iagooliveiradev@outlook.com>
2024-11-08 23:15:18 -0300

    style: ordenação dos cards

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -131,8 +131,7 @@
                         ];
                     @endphp
 
-                    <ul
-                        class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                         @foreach ($tiposImagem as $tipo)
                             <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                                 <div class="flex items-center ps-3">
@@ -141,8 +140,7 @@
                                         name="stocks_type[]"
                                         class=" form-control-input w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
 
-                                    <label for="{{ $tipo['name'] . '-' . $tipo['id'] }}"
-                                        class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $tipo['name'] }}</label>
+                                    <label for="{{ $tipo['name'] . '-' . $tipo['id'] }}" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $tipo['name'] }}</label>
                                 </div>
                             </li>
                         @endforeach
@@ -201,8 +199,8 @@
                             class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesquisar</button>
                     </div>
                 </form>
-
-                <div class="w-[1200px] grid grid-rows-3 grid-flow-col gap-4">
+{{-- w-[1200px] grid grid-cols-3 grid-flow-row gap-4 --}}
+                <div class="w-[1200px] grid grid-cols-4 grid-flow-row gap-4">
                     @foreach ($historyData as $key => $content)
                         <x-box-image-history :content="$content" :key="$key" />
                     @endforeach
